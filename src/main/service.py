c4e390e8580ce3d532d60f9acfd55814123cc7bd
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-11-24 00:02:42 +0300

    Split `process_repo` func

--- src/main/service.py
+++ src/main/service.py
@@ -28,26 +28,45 @@ from pathlib import Path
 from django.template import Context, Template
 
 from main.algorithms import files_sorted_by_last_changes, files_sorted_by_last_changes_from_db
-from main.models import RepoConfig
 from main.services.github_objs.cloned_repo import ClonedRepo
 from main.services.github_objs.new_issue import NewIssue
 from main.services.revive_config.disk_revive_config import DiskReviveConfig
 from main.services.revive_config.merged_config import MergedConfig
 from main.services.revive_config.pg_revive_config import PgReviveConfig
 from main.services.revive_config.pg_updated_revive_config import PgUpdatedReviveConfig
+from main.services.revive_config.revive_config import ConfigDict
 from main.services.synchronize_touch_records import PgSynchronizeTouchRecords
+
+
+def _define_files_for_search(repo_path: Path, config: ConfigDict) -> list[Path]:
+    return [
+        x
+        for x in repo_path.glob(config['glob'] or '**/*')
+        if '.git' not in str(x)  # TODO .github/workflows dir case
+    ]
+
+
+def _sorted_file_list(repo_path: Path, file_list: dict[Path, int]):
+    return sorted(
+        [
+            (
+                str(path).replace(
+                    '{0}/'.format(repo_path),
+                    '',
+                ),
+                points,
+            )
+            for path, points in file_list.items()
+        ],
+        key=lambda x: (x[1], str(x[0])),
+        reverse=True,
+    )
 
 
 def process_repo(repo_id: int, cloned_repo: ClonedRepo, new_issue: NewIssue):
     """Processing repo."""
-    repo_config = RepoConfig.objects.get(repo_id=repo_id)
     with tempfile.TemporaryDirectory() as tmpdirname:
         repo_path = cloned_repo.clone_to(Path(tmpdirname))
-        files_for_search = [
-            x
-            for x in repo_path.glob(repo_config.files_glob or '**/*')
-            if '.git' not in str(x)
-        ]
         config = PgUpdatedReviveConfig(
             repo_id,
             MergedConfig.ctor(
@@ -57,23 +76,13 @@ def process_repo(repo_id: int, cloned_repo: ClonedRepo, new_issue: NewIssue):
         ).parse()
         got = files_sorted_by_last_changes_from_db(
             repo_id,
-            files_sorted_by_last_changes(repo_path, files_for_search),
+            files_sorted_by_last_changes(
+                repo_path,
+                _define_files_for_search(repo_path, config),
+            ),
             repo_path,
         )
-    stripped_file_list: list[tuple[str, int]] = sorted(
-        [
-            (
-                str(path).replace(
-                    '{0}/'.format(repo_path),
-                    '',
-                ),
-                points,
-            )
-            for path, points in got.items()
-        ],
-        key=lambda x: (x[1], str(x[0])),
-        reverse=True,
-    )[:config['limit']]
+    stripped_file_list: list[tuple[str, int]] = _sorted_file_list(repo_path, got)[:config['limit']]
     file_list: list[str] = [file for file, _ in stripped_file_list]
     new_issue.create(
         'Issue from revive-code-bot',
