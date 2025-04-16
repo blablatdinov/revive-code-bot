a215e7ef6c995357900fa6d46afee579685abaa5
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-04-16 23:32:38 +0300

    lint

--- src/main/management/commands/worker.py
+++ src/main/management/commands/worker.py
@@ -55,7 +55,9 @@ class Command(BaseCommand):
         process_task_record = ProcessTask.objects.get(id=data['data']['process_task_id'])
         repo = process_task_record.repo
         try:
-            _task_processing_state(process_task_record)
+            process_task_record.status = ProcessTaskStatusEnum.in_process
+            process_task_record.traceback = ''
+            process_task_record.save()
             process_repo(
                 repo.id,
                 GhClonedRepo(repo),
