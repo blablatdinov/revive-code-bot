27fad51da2ace3b7775e165b27a3ef5d01e277e5
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-04-19 19:37:53 +0300

    simplify test

--- src/tests/main/test_service.py
+++ src/tests/main/test_service.py
@@ -44,9 +44,45 @@ def gh_repo(baker):
 
 
 @pytest.fixture
-def tmp_dir():
-    """
-    """
+def file_list() -> tuple[str, ...]:
+    return (
+        'CHANGELOG.md',
+        'Dockerfile',
+        'LICENSE',
+        'README.md',
+        'Taskfile.yml',
+        'go.mod',
+        'go.sum',
+        'pyproject.toml',
+        'renovate.json',
+        'src/cmd/gotemir.go',
+        'src/logic/cmprd_structures.go',
+        'src/logic/compared_structures.go',
+        'src/logic/config.go',
+        'src/logic/directory.go',
+        'src/logic/excluded_tests.go',
+        'src/logic/file_name_variants.go',
+        'src/logic/filter_out_from_config.go',
+        'src/logic/fk_directory.go',
+        'src/logic/fk_path.go',
+        'src/logic/os_directory.go',
+        'src/logic/path.go',
+        'src/logic/source_file_name_variants.go',
+        'src/logic/test_file_name_variants.go',
+        'tests/it/test.py',
+        'tests/unit/logic/cmprd_structures_test.go',
+        'tests/unit/logic/excluded_tests_test.go',
+        'tests/unit/logic/fk_path_test.go',
+        'tests/unit/logic/os_directory_test.go',
+        'tests/unit/logic/source_file_name_variants_test.go',
+        'tests/unit/logic/test_file_name_variants_test.go',
+        '.github/workflows/pr-check.yaml',
+        '.github/workflows/release.yaml',
+    )
+
+
+@pytest.fixture
+def tmp_dir(file_list):
     with tempfile.TemporaryDirectory() as tmpdirname:
         pth = Path(tmpdirname)
         Repo.init(pth)
@@ -55,41 +91,7 @@ def tmp_dir():
         (pth / 'tests/it/').mkdir(exist_ok=True, parents=True)
         (pth / 'tests/unit/logic').mkdir(exist_ok=True, parents=True)
         (pth / '.github/workflows').mkdir(exist_ok=True, parents=True)
-        files = (
-            'CHANGELOG.md',
-            'Dockerfile',
-            'LICENSE',
-            'README.md',
-            'Taskfile.yml',
-            'go.mod',
-            'go.sum',
-            'pyproject.toml',
-            'renovate.json',
-            'src/cmd/gotemir.go',
-            'src/logic/cmprd_structures.go',
-            'src/logic/compared_structures.go',
-            'src/logic/config.go',
-            'src/logic/directory.go',
-            'src/logic/excluded_tests.go',
-            'src/logic/file_name_variants.go',
-            'src/logic/filter_out_from_config.go',
-            'src/logic/fk_directory.go',
-            'src/logic/fk_path.go',
-            'src/logic/os_directory.go',
-            'src/logic/path.go',
-            'src/logic/source_file_name_variants.go',
-            'src/logic/test_file_name_variants.go',
-            'tests/it/test.py',
-            'tests/unit/logic/cmprd_structures_test.go',
-            'tests/unit/logic/excluded_tests_test.go',
-            'tests/unit/logic/fk_path_test.go',
-            'tests/unit/logic/os_directory_test.go',
-            'tests/unit/logic/source_file_name_variants_test.go',
-            'tests/unit/logic/test_file_name_variants_test.go',
-            '.github/workflows/pr-check.yaml',
-            '.github/workflows/release.yaml',
-        )
-        for fl in files:
+        for fl in file_list:
             (pth / fl).write_text('')
         yield pth
 
@@ -103,28 +105,13 @@ def test_process_repo_without_disk_config(gh_repo):
     )
 
 
-def test_define_files_files_count(tmp_dir):
+
+def test_define_files(tmp_dir, file_list):
     got = define_files_for_search(
         tmp_dir,
         {'glob': '**/*'},
     )
 
-    assert len(got) == 32
-
-
-def test_define_files_for_search_ignore_git_dir(tmp_dir):
-    got = define_files_for_search(
-        tmp_dir,
-        {'glob': '**/*'},
-    )
-
+    assert sorted(got) == sorted([tmp_dir / pth for pth in file_list])
     assert (tmp_dir / '.git/hooks/pre-commit.sample') not in got
-
-
-def test_define_files_for_search_contain_github_dir(tmp_dir):
-    got = define_files_for_search(
-        tmp_dir,
-        {'glob': '**/*'},
-    )
-
     assert (tmp_dir / '.github/workflows/pr-check.yaml') in got
