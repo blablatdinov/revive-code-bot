95b91252cc3cf656aa2c3777fbbc8aeeb38849c6
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2025-05-10 20:04:44 +0300

    Process task admin (#222)

--- src/main/admin.py
+++ src/main/admin.py
@@ -24,9 +24,30 @@
 
 from django.contrib import admin
 
-from main.models import GhRepo, ProcessTask, RepoConfig, TouchRecord
+from main.models import GhRepo, ProcessTask, ProcessTaskStatusEnum, RepoConfig, TouchRecord
 
 admin.site.register(GhRepo)
 admin.site.register(TouchRecord)
 admin.site.register(RepoConfig)
-admin.site.register(ProcessTask)
+
+
+@admin.register(ProcessTask)
+class ProcessTaskAdmin(admin.ModelAdmin):
+    """Admin panel for process task model."""
+
+    list_display = ('__str__', 'status', 'process_time', 'repo')
+    readonly_fields = ('formatted_created_at', 'formatted_updated_at')
+
+    def formatted_created_at(self, process_task: ProcessTask) -> str:
+        """Created at field."""
+        return process_task.created_at.isoformat()
+
+    def formatted_updated_at(self, process_task: ProcessTask) -> str:
+        """Updated at field."""
+        return process_task.updated_at.isoformat()
+
+    def process_time(self, process_task: ProcessTask) -> str:
+        """Calculate process time."""
+        if process_task.status != ProcessTaskStatusEnum.success:
+            return 'none'
+        return str(process_task.updated_at - process_task.created_at)
