b563be67bfd9016617caabb83c4d7a5a3dc4500f
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-06-03 15:36:07 +0300

    Reply to author

--- src/main/management/commands/worker.py
+++ src/main/management/commands/worker.py
@@ -1,5 +1,24 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023-2026 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
-# SPDX-License-Identifier: MIT
+# The MIT License (MIT).
+#
+# Copyright (c) 2023-2025 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
 
 """Worker for read process repo."""
 
@@ -12,10 +31,8 @@ from django.core.management.base import BaseCommand
 from django.db import close_old_connections
 from django.db.utils import OperationalError
 from django.utils import timezone
-from github.GithubException import GithubException
 
-from main.exceptions import UnavailableRepoError
-from main.models import ProcessTask, ProcessTaskStatusEnum, RepoStatusEnum
+from main.models import ProcessTask, ProcessTaskStatusEnum
 from main.service import process_repo
 from main.services.github_objs.gh_cloned_repo import GhClonedRepo
 from main.services.github_objs.gh_new_issue import GhNewIssue
@@ -32,7 +49,7 @@ class Command(BaseCommand):
     def handle(self, *args: list[str], **options: Any) -> None:  # noqa: ANN401
         """Entrypoint."""
         while True:
-            try:  # noqa: PLW0717
+            try:
                 process_task_record = (
                     ProcessTask.objects
                     .filter(status=ProcessTaskStatusEnum.pending)
@@ -43,7 +60,7 @@ class Command(BaseCommand):
                     sleep(2)
                     continue
                 repo = process_task_record.repo
-                try:  # noqa: PLW0717
+                try:
                     process_task_record.status = ProcessTaskStatusEnum.in_process
                     process_task_record.traceback = ''
                     process_task_record.updated_at = timezone.now()
@@ -58,25 +75,14 @@ class Command(BaseCommand):
                     process_task_record.updated_at = timezone.now()
                     process_task_record.traceback = ''
                     process_task_record.save()
-                except GithubException as err:
-                    if 'Issues has been disabled in this repository' in str(err):
-                        logger.exception('Issues has been disabled in this repository')
-                        repo.status = RepoStatusEnum.inactive
-                        repo.save()
-                        process_task_record.status = ProcessTaskStatusEnum.failed
-                        process_task_record.updated_at = timezone.now()
-                        process_task_record.traceback = traceback.format_exc() or ''
-                        process_task_record.save()
-                    else:
-                        raise
-                except UnavailableRepoError:
-                    logger.exception('Issues has been disabled in this repository')
-                    repo.status = RepoStatusEnum.inactive
-                    repo.save()
-                    process_task_record.status = ProcessTaskStatusEnum.failed
-                    process_task_record.updated_at = timezone.now()
-                    process_task_record.traceback = traceback.format_exc() or ''
-                    process_task_record.save()
+                    if process_task_record.trigger_issue_id:
+                        GhIssueComment(
+                            gh_repo,
+                            data['data']['trigger_issue_id'],
+                            # TODO: tag comment author
+                            # TODO: send link to new issue
+                            'Issue created',
+                        ).publish()
                 except Exception:
                     logger.exception('Fail process repo. Traceback: %s', traceback.format_exc())
                     process_task_record.status = ProcessTaskStatusEnum.failed
