ab34e17c6125c7ccc9b457222004a39589c2d35c
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2025-07-22 15:19:58 +0300

    Remove rabbitmq (#270)

--- src/main/views/process_repo.py
+++ src/main/views/process_repo.py
@@ -22,59 +22,17 @@
 
 """HTTP controller for process repo."""
 
-import datetime
-import json
 import logging
-import traceback
-import uuid
-from contextlib import closing
 
-import pika
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-from pika.exchange_type import ExchangeType
 
 from main.models import GhRepo, ProcessTask, ProcessTaskStatusEnum
 
 logger = logging.getLogger(__name__)
-
-
-def publish_event(event_data: dict) -> None:
-    """Publishing event in rabbitmq."""
-    try:
-        with closing(
-            pika.BlockingConnection(pika.ConnectionParameters(
-                virtual_host=settings.RABBITMQ_VHOST,
-                host=settings.RABBITMQ_HOST,
-                port=settings.RABBITMQ_PORT,
-                credentials=pika.PlainCredentials(
-                    settings.RABBITMQ_USER,
-                    settings.RABBITMQ_PASS,
-                ),
-            )),
-        ) as connection:
-            exchange_name = 'ordered_repos'
-            channel = connection.channel()
-            channel.exchange_declare(exchange=exchange_name, exchange_type=ExchangeType.direct, durable=True)
-            channel.basic_publish(
-                exchange=exchange_name,
-                body=json.dumps(event_data),
-                routing_key='ordered_repos',
-                properties=pika.BasicProperties(
-                    delivery_mode=2,
-                    content_type='application/json',
-                ),
-            )
-            logger.info('Message "%s" published', event_data)
-    except Exception:
-        logger.exception('Error on publishing message. Traceback: %s', traceback.format_exc())
-        task = ProcessTask.objects.get(id=event_data['data']['process_task_id'])
-        task.status = ProcessTaskStatusEnum.failed
-        task.traceback = traceback.format_exc()
-        task.save()
 
 
 @csrf_exempt
@@ -90,14 +48,6 @@ def process_repo_view(request: HttpRequest, repo_id: int) -> HttpResponse:
         repo=repo,
         status=ProcessTaskStatusEnum.pending,
     )
-    publish_event({
-        'event_id': str(uuid.uuid4()),
-        'event_version': 1,
-        'event_name': 'RepoOrdered',
-        'event_time': str(datetime.datetime.now(tz=datetime.UTC)),
-        'producer': 'revive_bot.django',
-        'data': {'process_task_id': process_task.id},
-    })
     return JsonResponse(
         {'process_task_id': process_task.id},
         status=201,
--- src/tests/main/views/test_process_repo.py
+++ src/tests/main/views/test_process_repo.py
@@ -22,11 +22,9 @@
 
 """Test process repo."""
 
-import json
 import re
 from pathlib import Path
 
-import pika
 import pytest
 from django.conf import settings
 
@@ -59,36 +57,6 @@ def mock_github(mock_http):
         status_code=403,
     )
     return mock_http
-
-
-@pytest.fixture
-def messages_in_queue():
-    def _messages_in_queue(queue_name):
-        messages = []
-        connection = None
-        try:
-            connection = pika.BlockingConnection(
-                pika.ConnectionParameters(host=settings.RABBITMQ_HOST, port=settings.RABBITMQ_PORT),
-            )
-            channel = connection.channel()
-            max_messages = 10
-            for _ in range(max_messages):
-                method_frame, header_frame, body = channel.basic_get(queue=queue_name, auto_ack=False)
-
-                if method_frame:
-                    messages.append({
-                        'body': json.loads(body.decode()),
-                        'headers': header_frame.headers if header_frame else None,
-                        'routing_key': method_frame.routing_key,
-                    })
-                    channel.basic_ack(method_frame.delivery_tag)
-                else:
-                    break
-            return messages
-        finally:
-            if connection and not connection.is_closed:
-                connection.close()
-    return _messages_in_queue
 
 
 @pytest.mark.integration
