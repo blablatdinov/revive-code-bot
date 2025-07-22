379fb619e3926b018d0085ae6c86a756857ed66b
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-07-22 15:53:42 +0300

    Update auth tokens

---
+++ src/main/management/commands/update_croniq_tokens.py
@@ -0,0 +1,78 @@
+# The MIT License (MIT).
+#
+# Copyright (c) 2023-2025 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""Command to update the token in all Croniq tasks."""
+
+import sys
+import requests
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from main.models import GhRepo, RepoConfig
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Updates the token in all Croniq tasks (Authorization header)'
+
+    def handle(self, *args, **options):
+        success, failed = 0, 0
+        for repo in GhRepo.objects.all():
+            try:
+                config = RepoConfig.objects.filter(repo=repo).first()
+                if not config:
+                    logger.warning('No config found for repository {0}'.format(repo.full_name))
+                    continue
+                name = 'repo_{0}'.format(repo.id)
+                croniq_auth_headers = {'Authorization': 'Basic {0}'.format(settings.CRONIQ_API_KEY)}
+                resp = requests.get(
+                    '{0}/api/v1/tasks?name={1}'.format(settings.CRONIQ_DOMAIN, name),
+                    headers=croniq_auth_headers,
+                    timeout=5,
+                )
+                resp.raise_for_status()
+                results = resp.json().get('results', [])
+                if not results:
+                    logger.warning('Task {0} not found in Croniq'.format(name))
+                    continue
+                task = results[0]
+                put_resp = requests.put(
+                    '{0}/api/v1/tasks/{1}'.format(settings.CRONIQ_DOMAIN, task["id"]),
+                    json={
+                        'name': name,
+                        'schedule': config.cron_expression,
+                        'url': task.get('url'),
+                        'headers': {
+                            'Authorization': 'Basic {0}'.format(settings.BASIC_AUTH_TOKEN),
+                        },
+                    },
+                    headers=croniq_auth_headers,
+                    timeout=5,
+                )
+                put_resp.raise_for_status()
+                logger.info('Token updated for {0} (id={1})'.format(repo.full_name, repo.id))
+                success += 1
+            except Exception as exc:
+                logger.error('Error for {0}: {1}'.format(repo.full_name, exc))
+                failed += 1
+        logger.info('Done! Success: {0}, failed: {1}'.format(success, failed)) 
